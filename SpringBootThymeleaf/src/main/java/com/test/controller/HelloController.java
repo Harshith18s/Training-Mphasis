ddce33291e6ff93d0638970c709caea18d9cf128
Harshith18s <161696414+Harshith18s@users.noreply.github.com>
2026-04-07 13:07:47 +0530

    Add files via upload

---
+++ SpringBootThymeleaf/src/main/java/com/test/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public String homepag(Model model) {
+		
+		model.addAttribute("msg", "Simple Thymeleaf example response");
+		return "home";
+		
+	}
+}
