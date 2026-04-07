2163483fb3f87c1ff040f9439b585c022e5fa72a
Harshith18s <161696414+Harshith18s@users.noreply.github.com>
2026-04-07 13:13:06 +0530

    Add files via upload

---
+++ SpringExp2/src/main/java/com/test/TestDemo.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		 ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+		 Employee obj=ctx.getBean("emp", Employee.class);
+		 System.out.println(obj);
+
+	}
+
+}
